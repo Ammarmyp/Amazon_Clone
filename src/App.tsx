3c04f502c98f85113cf4865f7887990b4bd399ee
Ammarmyp <ammarmyp@gmail.com>
2024-03-12 10:34:44 +0300

    Building the nav bars structure and stye

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <div className="app">Cleaning up</div>
       <Header />
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,39 @@
 import "../styles/header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const Header = () => {
-  return <div className="header">
-    <img className="header_logo" src="" alt="" />
-    <div className="header_search">
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon Logo"
+      />
+      <div className="header_search">
         <input type="text" className="search_input" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest </span>
+          <span className="header_optionLineTwo">Sign in </span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns </span>
+          <span className="header_optionLineTwo">& Orders </span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your </span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+        <div className="header_optionCart">
+          <ShoppingCartIcon />
+          <span className="header_optionLineTwo header_cartCount">0</span>
+        </div>
+      </div>
     </div>
-    <div className="header_nav">
-         <div className="header_option">
-            <span className="header_optionLineOne">Hello Guest </span>
-            <span className="header_optionLineOne">Sign in </span>
-         </div>
-         <div className="header_option">
-         <span className="header_optionLineOne">Returns  </span>
-            <span className="header_optionLineOne">Orders  </span>
-         </div>
-         <div className="header_option">
-         <span className="header_optionLineOne">Your </span>
-            <span className="header_optionLineOne">Prime</span>
-         </div>
-    </div>
-  </div>;
+  );
 };
 
 export default Header;
